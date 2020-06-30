696833706b18aa171b45122785a37c5bb58bde8f
ferrisbueller66 <github email address>
2020-06-30 09:04:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 4;
+
+let mod = 19 % 5
+
+let max = Math.max(6, 8, 12, 4, 6, 7, 9, 10, 20);
