67a9c35ba6f589356f3916ff02219850dc51d805
BenWix <benwichser@gmail.com>
2020-06-30 16:25:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *150) + 1
+
+const mod = 24 % 5
+
+const max = Math.max(1,2,3,4,5,6,20)
