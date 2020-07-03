753e54ff6d8b8a202d7bcaaa3e542ee05170e5fe
Diane Crame <dcrame@icloud.com>
2020-07-03 16:19:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor((Math.ceiling)*1) + 1;
+
+let mod = 8/2
+
+let max = Math.max(10,13,15,17,20);
