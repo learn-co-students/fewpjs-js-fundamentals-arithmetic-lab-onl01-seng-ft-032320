3c9c00b23b9c4b304465f24129d52dc8dcddf4a9
nisticism <nistacular@gmail.com>
2020-07-13 23:30:35 -0700

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // Write your code here
 let num1 = 62;
 let num2 = 1;
-let multiply = num1 * num2;
+const multiply = num1 * num2;
 
 const random = Math.floor(Math.random() * 10) + 1;
 
 const mod = 28%6;
 
-const max = 20;
+const max = Math.max(15,16,17,18,19,20);
