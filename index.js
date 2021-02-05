87556841fc4af1ebe62a552f7c4fd5b63629d9af
Beth Ruff <beth.ruff2@gmail.com>
2021-02-04 22:01:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 // Write your code here
-let multiply = 31 * 2;
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2
 
 let random = Math.random();
 
-let mod = 425 % 25;
+let mod = 14 % 5;
 
 let max = Math.max(15, 14, 20);
